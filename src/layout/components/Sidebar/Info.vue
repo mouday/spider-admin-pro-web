ea419c1b1bcfd090a5d56fa063820209f0931353
pengshiyu <1940607002@qq.com>
2021-01-27 15:40:15 +0800

    add version

---
+++ src/layout/components/Sidebar/Info.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="sidebar-info">
+    <div>
+      <a href="https://pypi.org/project/spider-admin-pro/">
+        <img
+          alt="GitHub stars"
+          src="https://img.shields.io/github/stars/mouday/spider-admin-pro.svg?style=social"
+        ></a>
+    </div>
+
+    <div style="margin-top:10px;">
+      <a href="https://github.com/mouday/spider-admin-pro">
+        <img
+          alt="Version"
+          src="https://img.shields.io/pypi/v/spider-admin-pro.svg"
+        ></a>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  props: [],
+
+  components: {},
+
+  data() {
+    return {};
+  },
+
+  computed: {},
+
+  methods: {
+    async getData() {},
+  },
+
+  created() {
+    this.getData();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.sidebar-info {
+  margin-bottom: 20px;
+  text-align: center;
+}
+</style>
--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,56 +1,70 @@
 <template>
-  <div :class="{'has-logo':showLogo}">
-    <logo v-if="showLogo" :collapse="isCollapse" />
-    <el-scrollbar wrap-class="scrollbar-wrapper">
-      <el-menu
-        :default-active="activeMenu"
+  <div>
+    <div :class="{'has-logo':showLogo}">
+      <logo
+        v-if="showLogo"
         :collapse="isCollapse"
-        :background-color="variables.menuBg"
-        :text-color="variables.menuText"
-        :unique-opened="false"
-        :active-text-color="variables.menuActiveText"
-        :collapse-transition="false"
-        mode="vertical"
-      >
-        <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path" />
-      </el-menu>
-    </el-scrollbar>
+      />
+      <el-scrollbar wrap-class="scrollbar-wrapper">
+
+        <el-menu
+          :default-active="activeMenu"
+          :collapse="isCollapse"
+          :background-color="variables.menuBg"
+          :text-color="variables.menuText"
+          :unique-opened="false"
+          :active-text-color="variables.menuActiveText"
+          :collapse-transition="false"
+          mode="vertical"
+        >
+          <sidebar-item
+            v-for="route in routes"
+            :key="route.path"
+            :item="route"
+            :base-path="route.path"
+          />
+        </el-menu>
+
+      </el-scrollbar>
+    </div>
+
+    <Info />
+
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import Logo from './Logo'
-import SidebarItem from './SidebarItem'
-import variables from '@/styles/variables.scss'
+import { mapGetters } from 'vuex';
+import Logo from './Logo';
+import Info from './Info.vue';
+import SidebarItem from './SidebarItem';
+import variables from '@/styles/variables.scss';
 
 export default {
-  components: { SidebarItem, Logo },
+  components: { SidebarItem, Logo, Info },
   computed: {
-    ...mapGetters([
-      'sidebar'
-    ]),
+    ...mapGetters(['sidebar']),
     routes() {
-      return this.$router.options.routes
+      return this.$router.options.routes;
     },
     activeMenu() {
-      const route = this.$route
-      const { meta, path } = route
+      const route = this.$route;
+      const { meta, path } = route;
       // if set path, the sidebar will highlight the path you set
       if (meta.activeMenu) {
-        return meta.activeMenu
+        return meta.activeMenu;
       }
-      return path
+      return path;
     },
     showLogo() {
-      return this.$store.state.settings.sidebarLogo
+      return this.$store.state.settings.sidebarLogo;
     },
     variables() {
-      return variables
+      return variables;
     },
     isCollapse() {
-      return !this.sidebar.opened
-    }
-  }
-}
+      return !this.sidebar.opened;
+    },
+  },
+};
 </script>
